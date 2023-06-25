b05053558384b45c1411edefbf7946a179fb39bb
ndalpe <ndalpe@gmail.com>
2023-06-24 23:05:14 -0400

    Added the tests for update snip method.

--- mod/snippet/tests/snips_test.php
+++ mod/snippet/tests/snips_test.php
@@ -76,18 +76,13 @@ class snips_test extends \advanced_testcase {
         );
         $this->assertEquals(1, $DB->count_records('snippet_categories', ['id' => $categoryid]));
 
-        $description = [
-            'text' => 'This is the description',
-            'format' => FORMAT_PLAIN
-        ];
-
         $snipdata = new \stdClass();
         $snipdata->id = $cm->id;
         $snipdata->categoryid = $categoryid;
         $snipdata->userid = $user->id;
         $snipdata->snipid = 0;
         $snipdata->name = 'This is the name';
-        $snipdata->description = $description;
+        $snipdata->intro = ['text' => 'This is the intro', 'format' => FORMAT_PLAIN];
         $snipdata->firstcategory = 'no';
         $snipdata->timecreated = $time;
         $snipdata->timemodified = $time;
@@ -102,7 +97,7 @@ class snips_test extends \advanced_testcase {
         $snipdata->userid = $user->id;
         $snipdata->snipid = 0;
         $snipdata->name = 'This is the name';
-        $snipdata->description = $description;
+        $snipdata->intro = ['text' => 'This is the intro', 'format' => FORMAT_PLAIN];
         $snipdata->firstcategory = 'no';
         $snipdata->timecreated = $time;
         $snipdata->timemodified = $time;
@@ -144,17 +139,12 @@ class snips_test extends \advanced_testcase {
 
         $this->assertEquals(0, $DB->count_records('snippet_categories'));
 
-        $description = [
-            'text' => 'This is the description',
-            'format' => FORMAT_PLAIN
-        ];
-
         $snipdata = new \stdClass();
         $snipdata->id = $cm->id;
         $snipdata->userid = $user->id;
         $snipdata->snipid = 0;
         $snipdata->name = 'This is the name';
-        $snipdata->description = $description;
+        $snipdata->intro = ['text' => 'This is the intro', 'format' => FORMAT_PLAIN];
         $snipdata->firstcategory = 'yes';
         $snipdata->categoryname = 'This is the category';
         $snipdata->timecreated = $time;
@@ -196,7 +186,6 @@ class snips_test extends \advanced_testcase {
 
         // Create a user.
         $user1 = $generator->create_user();
-        $user2 = $generator->create_user();
 
         // Create 2 categories.
         $category1id = $snippetgenerator->create_category(
@@ -215,35 +204,46 @@ class snips_test extends \advanced_testcase {
         $snipdata->categoryid = $category1id;
         $snipdata->userid = $user1->id;
         $snipdata->snipid = 0;
-        $snipdata->name = 'First name';
-        $snipdata->description = ['text' => 'First description', 'format' => FORMAT_PLAIN];
+        $snipdata->name = 'Initial name';
+        $snipdata->intro = ['text' => 'Initial description', 'format' => FORMAT_PLAIN];
         $snipdata->firstcategory = 'no';
+        $snipdata->private = '1';
+        $snipdata->language = 'PHP';
+        $snipdata->snippet = '<?php php_info(); ?>';
         $snipdata->timecreated = $time;
         $snipdata->timemodified = $time;
 
-        $snip1id = snips::create($snipdata);
-        $this->assertEquals(1, $DB->count_records('snippet_snips', ['id' => $snip1id]));
+        $snipid = snips::create($snipdata);
 
-        // Update the snip.
-        $snipdata = new \stdClass();
-        $snipdata->snippetid = $snippet->id;
+        // Make sure the snip was created.
+        $this->assertEquals(1, $DB->count_records('snippet_snips', ['id' => $snipid]));
+
+        // Compare the data from $snipdata with the data from the database.
+        $createdsnip = $DB->get_record('snippet_snips', array('id' => $snipid));
+        foreach ($snipdata as $key => $value) {
+            $this->assertEquals($value, $createdsnip->{$key});
+        }
+
+        // Update the snip data.
+        $snipdata->snipid = $snipid;
         $snipdata->categoryid = $category2id;
-        $snipdata->userid = $user2->id;
-        $snipdata->snipid = 0;
         $snipdata->name = 'Updated name';
-        $snipdata->description = ['text' => 'Updated description', 'format' => FORMAT_PLAIN];
+        $snipdata->intro = ['text' => 'Updated description', 'format' => FORMAT_PLAIN];
+        $snipdata->private = '0';
+        $snipdata->language = 'javascript';
+        $snipdata->snippet = 'export default SELECTORS;';
+        $snipupdate = snips::update($snipdata);
+        $this->assertTrue(boolval($snipupdate));
 
-        $snip1id = snips::update($snipdata);
-        $this->assertEquals(1, $DB->count_records('snippet_snips', ['id' => $snip1id]));
-        $this->assertIsInt($snip1id);
-
-        // Get the updated snippet data.
-        $updatedsnip = $DB->get_record('snippet', array('id' => $snip1id));
+        // Get the updated snip data.
+        $updatedsnip = $DB->get_record('snippet_snips', array('id' => $snipid));
 
         // Make sure the snippet data was updated.
         $this->assertEquals($category2id, $updatedsnip->categoryid);
-        $this->assertEquals($user2->id, $updatedsnip->userid);
-        $this->assertEquals('Updated name', $updatedsnip->name);
-        $this->assertEquals('Updated description', $updatedsnip->intro);
+        $this->assertEquals($snipdata->name, $updatedsnip->name);
+        $this->assertEquals($snipdata->intro['text'], $updatedsnip->intro);
+        $this->assertEquals($snipdata->private, $updatedsnip->private);
+        $this->assertEquals($snipdata->language, $updatedsnip->language);
+        $this->assertEquals($snipdata->snippet, $updatedsnip->snippet);
     }
 }
